d6d85bd237e11d522ce50d176e27c76cdc00719c
gazwald <gazwald@users.noreply.github.com>
2024-06-05 11:03:45 +1000

    Fixes after 0.10 update

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -14,7 +14,7 @@ vim.o.ignorecase = true                 -- ignore case when searching
 vim.o.hlsearch = true                   -- highlight all pervious search pattern
 
 vim.o.cursorline = true                 -- highlight current cursor position
-vim.o.colorcolumn =120                  -- display column at 120 characters
+vim.o.colorcolumn = '+1'                -- display column at 120 characters
 
 vim.o.wrap = false                      -- do not wrap lines by default
 
@@ -23,12 +23,12 @@ vim.o.tabstop = 2                       -- how many spaces does \t equal
 vim.o.shiftwidth = 2                    -- how many spaces is an indentation
 vim.o.softtabstop = 2
 vim.o.smartindent = true                -- auto indent on new lines, see :help smartindent
-vim.o.showmatch =true                   -- display matching bracket or parenthesis
+vim.o.showmatch = true                  -- display matching bracket or parenthesis
 
 vim.o.spelllang = 'en_au'               -- spellcheck default language
-vim.o.spellsuggest = 'best,5'            -- suggest 5 best words
+vim.o.spellsuggest = 'best,5'           -- suggest 5 best words
 
-vim.o.mouse = false                     -- disable mouse support
+vim.o.mouse = ''                        -- disable mouse support
 
 vim.o.updatetime = 100                  -- 100ms instead of default 4000ms
 

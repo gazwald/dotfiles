0b9f9dbc6538c0e02c2126c26986cc829e303745
Gary Brandon <gary.brandon@rea-group.com>
2024-08-26 09:09:22 +1000

    Misc updates from Friday

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    /opt/homebrew/bin/sketchybar --set "$NAME" background.drawing=on
 else
-    sketchybar --set $NAME background.drawing=off
+    /opt/homebrew/bin/sketchybar --set "$NAME" background.drawing=off
 fi
